98227165a6d01a21175ab958df9c1f3d0f76cda0
aniketpandey077 <aniketpandey301271@gmail.com>
2026-07-11 12:22:09 +0530

    Configure custom UniHud favicon and PWA assets

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,12 +5,13 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#FF4757" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Campus Connect" />
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <meta name="apple-mobile-web-app-title" content="UniHud" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
       </Head>
       <body className="antialiased">
         <Main />
